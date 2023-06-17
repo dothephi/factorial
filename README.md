74242a09da0d04fc96ae79fbae7ffc512b49222b
dothephi <phidtse161229@fpt.edu.vn>
2023-06-17 11:52:58 +0700

    first upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Welcome to Math Ultility Repository
+
+## You will find in this repo...
+
+* Coming soon...
+
+##### Copyright &#169; 2023 darki.phi
---
+++ src/main/java/com/darki/factorial/main/Factorial.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.darki.factorial.main;
+
+import com.darki.mathutil.core.MathUtility;
+
+/**
+ *
+ * @author MSI
+ */
+public class Factorial {
+    
+    public static void main(String[] args) {
+        System.out.println("5! = " + MathUtility.getFactorial(5));
+    }
+}
